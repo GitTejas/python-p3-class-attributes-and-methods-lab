5bf0b41ea005cc3dc74e44c369b10789dfd887fe
Tejas Nampalli <TSNampalli@gmail.com>
2024-05-03 15:27:10 -0400

    done

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,91 @@
 class Song:
-    pass
+
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+    
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.add_song_to_count()
+        Song.add_to_genres(self)
+        Song.add_to_artists(self)
+        Song.add_to_genre_count(self)
+        Song.add_to_artist_count(self)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, song):
+        if song.genre not in cls.genres:
+            cls.genres.append(song.genre)
+
+    @classmethod
+    def add_to_artists(cls, song):
+        if song.artist not in cls.artists:
+            cls.artists.append(song.artist)
+
+    @classmethod
+    def add_to_genre_count(cls, song):
+        if song.genre in cls.genre_count:
+            cls.genre_count[song.genre] += 1
+        else:
+            cls.genre_count[song.genre] = 1
+        
+    @classmethod
+    def add_to_artist_count(cls, song):
+        if song.artist in cls.artist_count:
+            cls.artist_count[song.artist] += 1
+        else:
+            cls.artist_count[song.artist] = 1
+
+
+########################## OTHER ############################
+"""
+class Song:
+    count = 0
+    genres = []
+    genre_count = {}
+    artists = []
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.add_song_to_count()
+        self.add_to_genres()
+        self.add_to_artists()
+        self.add_to_genre_count()
+        self.add_to_artist_count()
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    def add_to_genres(self):
+        if self.genre not in Song.genres:
+            Song.genres.append(self.genre)
+
+    def add_to_artists(self):
+        if self.artist not in Song.artists:
+            Song.artists.append(self.artist)
+
+    def add_to_genre_count(self):
+        if self.genre in Song.genre_count:
+            Song.genre_count[self.genre] += 1
+        else:
+            Song.genre_count[self.genre] = 1
+
+    def add_to_artist_count(self):
+        if self.artist in Song.artist_count:
+            Song.artist_count[self.artist] += 1
+        else:
+            Song.artist_count[self.artist] = 1
+
+            """
